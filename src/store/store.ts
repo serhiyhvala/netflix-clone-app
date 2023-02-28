081906c5c56aba199f2b34ef9bf3a89d6562425d
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-02-28 20:49:38 +0200

    Add Store

---
+++ src/store/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import { moviesApi } from '../services/moviesApi'
+
+export const store = configureStore({
+	reducer: {
+		[moviesApi.reducerPath]: moviesApi.reducer
+	},
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware().concat(moviesApi.middleware)
+})
