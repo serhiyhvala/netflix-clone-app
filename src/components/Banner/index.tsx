01b2d5daf6a19568c4e836140988dd50f5f7251f
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-02-28 16:01:19 +0200

    Fetch Data For Banner Component

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,29 +1,50 @@
+import { useEffect, useState } from 'react'
+
+import { IMovie } from '../../services/getAllMovies.types'
+import { useGetNetflixOriginalsQuery } from '../../services/moviesApi'
+
 import './Banner.scss'
 
 const Banner = () => {
+	const { data } = useGetNetflixOriginalsQuery('tv')
+
+	const [movie, setMovies] = useState<IMovie | undefined>()
+
 	const truncate = (string: string, n: number): string => {
 		return string?.length > n ? string.substring(0, n - 1) + '...' : string
 	}
+
+	const randomNumber =
+		data && Math.floor(Math.random() * data?.results.length - 1)
+
+	const pointForImage = 'https://image.tmdb.org/t/p/original'
+
+	useEffect(() => {
+		randomNumber && setMovies(data?.results[randomNumber])
+	}, [data, randomNumber])
+
 	return (
 		<header
 			className='banner'
 			style={{
 				backgroundSize: 'cover',
-				backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/c/cd/Black_flag.svg")`,
-				backgroundPosition: 'center center'
+				backgroundImage: `url(${pointForImage}/${movie?.backdrop_path})`,
+				backgroundPosition: 'center'
 			}}
 		>
 			<div className='banner__contents'>
-				<h1 className='banner__title'>Movie Name</h1>
+				<h1 className='banner__title'>
+					{movie?.name || movie?.original_name || movie?.title}
+				</h1>
 				<div className='banner__buttons'>
 					<button className='banner__button'>Play</button>
 					<button className='banner__button'>My List</button>
 				</div>
 				<div className='banner__description'>
-					{truncate('This is a test description', 120)}
+					{movie?.overview && truncate(movie.overview, 150)}
 				</div>
-				<div className='banner__fadeBottom'></div>
 			</div>
+			<div className='banner__fadeBottom'></div>
 		</header>
 	)
 }
