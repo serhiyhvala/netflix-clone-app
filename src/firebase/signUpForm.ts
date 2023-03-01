c2172e460e6949ce8cf56163f22977359d8cfc5f
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-03-01 13:22:35 +0200

    Update SignUp Form

--- src/firebase/signUpForm.ts
+++ src/firebase/signUpForm.ts
@@ -6,13 +6,9 @@ import {
 import { auth } from './firebase'
 
 export const createUser = async (email: string, password: string) => {
-	await createUserWithEmailAndPassword(auth, email, password)
-		.then(authUser => {
-			console.log(authUser)
-		})
-		.catch(error => {
-			console.error(error)
-		})
+	await createUserWithEmailAndPassword(auth, email, password).catch(error => {
+		console.error(error)
+	})
 }
 
 export const signInUser = async (emailRef: string, passwordRef: string) => {
