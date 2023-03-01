69406044e1102160bdae40612bbdc3e2e9bf0044
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-03-01 13:23:11 +0200

    Add isLoginUserData for initial state, and for data, when user is Log In

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -1,19 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+
+export type isLoginUserData = {
+	email: string | null
+	uid: string | null
+}
 
 export type InitialStateType = {
 	isLogin: boolean
+	isLoginUserData: isLoginUserData
 }
 
 const initialState: InitialStateType = {
-	isLogin: false
+	isLogin: false,
+	isLoginUserData: { email: '', uid: '' }
 }
 
 export const userSlice = createSlice({
 	name: 'userSlice',
 	initialState,
 	reducers: {
-		loginUser: (state: InitialStateType, action) => {
-			state.isLogin = action.payload
+		loginUser: (
+			state: InitialStateType,
+			action: PayloadAction<isLoginUserData>
+		) => {
+			state.isLoginUserData = {
+				...state.isLoginUserData,
+				email: action.payload.email,
+				uid: action.payload.uid
+			}
+			state.isLogin = true
 		},
 		logoutUser: state => {
 			state.isLogin = false
